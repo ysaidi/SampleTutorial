63139b2c558f0fa7bb397007814d42e91106507d
ysaidi <ysaidi@RUITD-560.smsaexpress.com>
2017-12-12 15:01:11 +0300

    Initial get version

---
+++ BlogGitTest/src/de/hahn/blog/gittest/GitTestMain.java
@@ -0,0 +1,11 @@
+package de.hahn.blog.gittest;
+
+public class GitTestMain {
+    public GitTestMain() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        GitTestMain gitTestMain = new GitTestMain();
+    }
+}
