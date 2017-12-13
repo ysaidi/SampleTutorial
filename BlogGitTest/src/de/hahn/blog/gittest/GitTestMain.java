5c753c7d24b38b05571a71ce5b97568933210d2d
ysaidi <34507732+ysaidi@users.noreply.github.com>
2017-12-13 14:52:12 +0300

    Update GitTestMain.java
    Change jus to see how to pull

--- BlogGitTest/src/de/hahn/blog/gittest/GitTestMain.java
+++ BlogGitTest/src/de/hahn/blog/gittest/GitTestMain.java
@@ -12,6 +12,6 @@ public class GitTestMain {
     public static void main(String[] args) {
         GitTestMain gitTestMain = new GitTestMain();
         
-        System.out.println("For test run - no actions");
+        System.out.println("For test run - no actions-----");
     }
 }
