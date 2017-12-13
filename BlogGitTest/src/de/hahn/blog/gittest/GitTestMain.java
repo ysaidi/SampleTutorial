98ad5fe346ad5dbf4beda3872456056c73e21492
ysaidi <ysaidi@RUITD-560.smsaexpress.com>
2017-12-13 14:48:50 +0300

    Change in Main method

--- BlogGitTest/src/de/hahn/blog/gittest/GitTestMain.java
+++ BlogGitTest/src/de/hahn/blog/gittest/GitTestMain.java
@@ -11,5 +11,7 @@ public class GitTestMain {
 
     public static void main(String[] args) {
         GitTestMain gitTestMain = new GitTestMain();
+        
+        System.out.println("For test run - no actions");
     }
 }
