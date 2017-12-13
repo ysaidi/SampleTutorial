dba7039bc74730086cbdae2c9c152f2e4489bae3
ysaidi <ysaidi@RUITD-560.smsaexpress.com>
2017-12-13 08:58:55 +0300

    Calculator Class Has Added

---
+++ BlogGitTest/src/de/hahn/blog/gittest/Calculator.java
@@ -0,0 +1,14 @@
+package de.hahn.blog.gittest;
+
+public class Calculator {
+    public Calculator() {
+        super();
+    }
+    
+public Integer add(Integer param1,Integer param2){
+        if(param1!=null && param2!=null)
+            return param1 + param2;
+        else
+            return null;
+    }
+}
